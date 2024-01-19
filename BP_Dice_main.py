9fa3e9d20e6ea0117519eaef63f89a2ada605e42
SHELL <south02274653@gmail.com>
2024-01-19 23:41:55 +0900

    バージョンの表記とコマンド説明文を追加
    - ウインドウのタイトルバーにタイトルとバージョンを表示するようにした
    - コマンドの説明文を表示するようにした

--- BP_Dice_main.py
+++ BP_Dice_main.py
@@ -1,12 +1,27 @@
 import discord
+import os
 import random
 
 import BP_Dice_resource
 
 bot = discord.Bot()
 
-@bot.slash_command(guild_ids=[BP_Dice_resource.ServerID])
+def title(text):
+    # OSの種類を判別する
+    # Windows
+	if os.name == 'nt':
+		os.system(f'title {text}')
+    # Mac / Linux
+	elif os.name == 'posix':
+		print(f'\x1b]2;{text}\x07', end='', flush=True)
+
+@bot.event
+async def on_ready():
+	await bot.change_presence(activity=discord.Game(name="/dice"))
+
+@bot.slash_command(description="ダイスを振ります(範囲：0～999)")
 async def dice(ctx):
 	await ctx.respond('ダイス！【{}】'.format(random.randint(0, 999)))
 
+title("BP_Dice v1.0.0")
 bot.run(BP_Dice_resource.DiscordBotToken)
