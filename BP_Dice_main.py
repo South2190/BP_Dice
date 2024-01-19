2db1e61b23351f2d33cedc9be8a494c723486949
SHELL <south02274653@gmail.com>
2024-01-19 20:23:19 +0900

    最低限の形への仕上げとServerIDの記述の分離
    - /diceコマンドがとりあえず動くようにした
    - ソースコードに直接記述していたServerIDを'BP_Dice_resource.py'で記述する仕様に変更

--- BP_Dice_main.py
+++ BP_Dice_main.py
@@ -1,11 +1,12 @@
 import discord
+import random
+
 import BP_Dice_resource
 
 bot = discord.Bot()
 
-@bot.slash_command(guild_ids=[889855802054156288])
-async def aping(ctx):
-	print("catch aping")
-	await ctx.respond('apong')
+@bot.slash_command(guild_ids=[BP_Dice_resource.ServerID])
+async def dice(ctx):
+	await ctx.respond('ダイス！【{}】'.format(random.randint(0, 999)))
 
 bot.run(BP_Dice_resource.DiscordBotToken)
