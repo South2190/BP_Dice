f4653a6145873e5bdcc1531ac8e0f5d86d900106
SHELL <south02274653@gmail.com>
2024-01-22 11:59:05 +0900

    macからのスクリプトを用いたcommit&pushテスト
    - mac用のcommit&pushスクリプトをつくった

--- Commit_batch/Commit.bat
+++ Commit_batch/Commit.bat
@@ -1,17 +1,19 @@
 @echo off
+
 cd %~dp0\..
 
 git pull origin master
+python Commit_batch/rewrite_json.py
 git add .
 git commit -a
 
 :confirmation
-set yn = nul
 set /p yn="masterƒuƒ‰ƒ“ƒ`‚Öpush‚µ‚Ü‚·‚©?(y/n)>"
 if '%yn%' == 'y' (
 	git push origin master
 ) else if not '%yn%' == 'n' (
 	goto confirmation
 )
+
 pause
 goto :eof
---
+++ Commit_batch/Commit.command
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd `dirname $0`/..
+
+git pull origin master
+python Commit_batch/rewrite_json.py
+git add .
+git commit -a
+
+while true
+do
+echo -n "masterブランチへpushしますか?(y/n)>"
+read Slt
+
+if [ ${Slt} = "y" ]; then
+	git push origin master
+	break
+elif [ ${Slt} = "n" ]; then
+	break
+fi
+done
+
+read
