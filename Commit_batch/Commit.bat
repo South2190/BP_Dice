e83129dde7abc1fe973ea06c6a420cd67353305a
SHELL <south02274653@gmail.com>
2024-03-02 00:06:05 +0900

    Commitバッチの変更
    - タグの作成を行うようにした
    - プル、プッシュ等の操作をpython側で行うようにした

--- Commit_batch/Commit.bat
+++ Commit_batch/Commit.bat
@@ -1,44 +1,7 @@
 @echo off
 
-set Branch=dev
-
-cd %~dp0\..
-
-git pull origin %Branch%
-
-:createtag
-set /p Slt="tagを作成しますか?(y/n)>"
-if '%Slt%' == 'y' (
-	setlocal enabledelayedexpansion
-	call:setversion
-	python Commit_batch/rewrite_json.py tag !Ver!
-) else if '%Slt%' == 'n' (
-	python Commit_batch/rewrite_json.py
-) else goto createtag
-pause
-git add .
-git commit -a
-goto confirmation
-
-:setversion
-set /p Ver="バージョンを入力>"
-if not defined Ver goto setversion
-exit /b
-
-:confirmation
-set Slt=
-set /p Slt="%Branch%ブランチへpushしますか?(y/n)>"
-if '%Slt%' == 'y' (
-	git push origin %Branch%
-) else if not '%Slt%' == 'n' (
-	goto confirmation
-)
-
-if defined Ver (
-	git tag -a !Ver!
-	git push origin !Ver!
-	endlocal
-)
+cd %~dp0
+python rewrite_json.py
 
 echo 完了しました
 pause
--- Commit_batch/Commit.command
+++ Commit_batch/Commit.command
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-cd `dirname $0`/..
-
-git pull origin dev
-python Commit_batch/rewrite_json.py
-git add .
-git commit -a
-
-while true
-do
-echo -n "devブランチへpushしますか?(y/n)>"
-read Slt
-
-if [ ${Slt} = "y" ]; then
-	git push origin dev
-	break
-elif [ ${Slt} = "n" ]; then
-	break
-fi
-done
+cd `dirname $0`
+python rewrite_json.py
 
 echo "完了しました"
 read
